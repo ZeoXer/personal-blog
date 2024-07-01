b396e116370e732a7d4cc4483f55e03f5da54ceb
ZeoXer <andrew17413@gmail.com>
2024-07-01 15:49:37 +0800

    modify next config image remote path

--- next.config.js
+++ next.config.js
@@ -15,6 +15,11 @@ const nextConfig = {
         port: '8080',
         pathname: '/uploadImgs/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'zeoxer.com',
+        pathname: '/uploadImgs/**',
+      },
     ],
   },
 };
